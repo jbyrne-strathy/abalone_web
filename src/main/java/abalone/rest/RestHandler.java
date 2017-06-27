3c73eccffd4f49d75ea493d7357f63e311d118b3
jbyrne-strathy <james.byrne-1@outlook.com>
2017-06-27 23:31:14 +0200

    Disabled Rest controller, updated to latest spring boot. Added Facebook JS and script tag options for yui-compressed scripts.

--- src/main/java/abalone/rest/RestHandler.java
+++ src/main/java/abalone/rest/RestHandler.java
@@ -1,22 +1,18 @@
 package abalone.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by james on 20/06/17.
  */
 @RestController
 public class RestHandler {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/restRequest")
-    public RestResponse response(@RequestParam(value="name", defaultValue="World") String name) {
-        return new RestResponse(counter.incrementAndGet(),
-                String.format(template, name));
-    }
+//    private static final String template = "Hello, %s!";
+//    private final AtomicLong counter = new AtomicLong();
+//
+//    @RequestMapping("/restRequest")
+//    public RestResponse response(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new RestResponse(counter.incrementAndGet(),
+//                String.format(template, name));
+//    }
 }
